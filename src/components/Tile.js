f3e26fefe42ff6484eb269faa539a5f8511959a6
Katherine de Vay <katdevay2@gmail.com>
2023-02-20 22:35:13 -0600

    Debug increment/decrement function

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,20 +2,18 @@
 import React from 'react';
 
 export default function Tile(props) {
-    const {increment, addToCart, fruit, index} = props;
-    const {source, alt, name, count} = fruit;
-
+    const {increment, fruit, index} = props; //addToCart
 
     return (
         <div className="tile"> 
-            <img src={source} alt={alt}></img>
-            <p>{name}</p>
+            <img className='tileImages' src={fruit.src} alt={fruit.alt}></img>
+            <p>{fruit.name}</p>
             <span>
                 <button data={index} id='p' onClick={increment}>+</button>
-                <a>{count}</a>
+                <a>{fruit.count}</a>
                 <button data={index} id='m' onClick={increment}>-</button>
             </span>
-            <button onClick={addToCart}>Add to cart</button>
+            <button>Add to cart</button>
         </div>
     );
 }
